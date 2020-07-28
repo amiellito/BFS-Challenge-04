054f1ea000079388543de5a13284e9ec6883e63e
amiellito <amiel.lapuebla@gmail.com>
2020-07-28 14:19:26 -0700

    established boilerplate - mongoose

--- app.js
+++ app.js
@@ -5,6 +5,31 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/<dbname>", {useNewParser:true, useUnifiedTopology:true});
+
+const catSchema = {
+  user_id: String,
+  url: String,
+  vote: Number
+};
+
+const Cat = mongoose.model("Image", catSchema);
+
+
+var catArr = [] //push voted cat images here. 
+
+//loop through catArr and display cat images
+
+/////////////////////////ROUTES////////////////////////
+
+
+
+
 app.listen(3000, function(){
   console.log("Listen port 3000")
 });
